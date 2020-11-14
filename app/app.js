8ebac7e8fbc33f50b7a8f8b6d04d251d1203db3f
The-Unknown-Developer <theunknowndevloper@gmail.com>
2020-11-14 13:58:56 -0500

    user auth updated

--- app/app.js
+++ app/app.js
@@ -1,11 +1,24 @@
-
+const session = require("express-session")
 const bodyParser = require('body-parser')
 const router = require('./routes.js')
 const express = require('express')
 const app = express()
 
 
+const sessionConfig = {
+    saveUninitialized : true,
+    secret: "secret",
+    resave: false,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge:  1000 * 60 * 60 * 24 * 7, 
+        httpOnly: true
+    }
+}
+
+
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session(sessionConfig))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use('/', router)
--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,15 @@
 
-const {message, statusCode} = require("../utils/expressError.js")
+const {validateUser} = require("../middleware/validate.js")
+const ExpressError = require("../utils/expressError.js")
 const catchAsync = require("../utils/catchAsync")
 const DB = require('../database/connection.js')
-const express = require('express')
-const ExpressError = require("../utils/expressError.js")
-const bcrypt = require('bcrypt');
+const session = require("express-session")
 const Prevention = require('sqlstring')
+const express = require('express')
+const bcrypt = require('bcrypt')
 const route = express.Router()
-const {validateUser} = require("../middleware/validate.js")
+
+
 
 
 
@@ -18,41 +20,31 @@ route.get('/', (req, res) => {
         if(err) {
             console.log(err)
         } else {
-            
             res.render('home.ejs', {movies})
     
         }
-    
-    })  
-   
+    })   
 })
 
 // show
 
 route.get('/movie/:id',  (req, res) => {
-    let id = req.params.id
-   
+    let id = req.params.id  
         DB.query(`SELECT * FROM flixers WHERE id = "${id}" LIMIT 1`, (err, movie) => {
             if(err || movie.length === 0 ) {
                 console.log(err)
                 res.render("error.ejs", {err})
                
             }  else {
-               
                 res.render("show.ejs", {movie})
             }
-    
-        })
-
-    
-   
+        }) 
 })
 
 // sign up
 
 
 route.get('/signup', (req, res) => {
-
     res.render('sign_up.ejs')
 })
 
@@ -64,56 +56,40 @@ route.post("/signup", validateUser, catchAsync( async (req, res, next) => {
     VALUES (${Prevention.escape(user.f_name)},
             ${Prevention.escape(user.l_name)},
             ${Prevention.escape(user.email)},
-            ${Prevention.escape(password)})`, (err, result) => {
+            ${Prevention.escape(password)})`, catchAsync( async (err, account) => {
                 if(err) {
                   res.render('error.ejs', {err})
                 } else {
-                  
                     res.redirect("/")
                 }
-
-            })
+            }))
 }))
 
 // log in
 
-
 route.get('/login', (req, res) => {
-
-  
     res.render('log_in.ejs')
 })
 
 
 route.post('/login', (req, res) => {
     const {user} = req.body
-
     DB.query(`SELECT * FROM users WHERE email = "${user.email}"`, async (err, account) => {
-
         try {
             if(account[0].email === user.email) {
-               const match = await bcrypt.compare(user.password, account[0].password)
+                const validPassword = await bcrypt.compare(user.password, account[0].password)
 
-               if(match === true) {
-                   res.send("signed in ")
-               } else {
-                   res.send("inncorect password or username")
-               }
-               
+                if(validPassword === true) {
+                    req.session.user = account[0].id
+                    res.redirect("/")
+                } else {
+                    res.send("inncorect password or username")
+                }           
             } 
-
-        
-
         } catch (e) {
-
             res.send("inccorect password or username")
-
-        }
-       
-       
-       
+        }   
     })    
-
 })
 
 
@@ -129,7 +105,20 @@ route.post('/login', (req, res) => {
 
 
 
-
+route.get("/secret", async (req, res) => {
+    try {
+        if( !req.session.user) {
+            res.redirect("/")
+            
+        }
+        res.send("this is secret ! You cannot see me unless you are")
+    } catch(e) {
+        console.log(e)
+    
+    }
+  
+   
+})
 
 
 
